44281f000fd57deea8b3875050b65857ab2e0183
Aaron Roller <noraarellor@gmail.com>
2018-11-05 12:18:57 -0600

    Made some radical changes to how data is stored in our system

--- js/main.js
+++ js/main.js
@@ -1,65 +1,82 @@
 $(document).ready( function() {
+    init();
 
-init();     // populate the menu etc....
+    // ---------------------------------
+    //      button pressing code
+    // ---------------------------------
 
-// When a menu item in the menu is clicked
-$('.menu-area .item').on("click", function() {
+    // When a menu item in the menu is clicked
+    $('.menu-area .item').on("click", function() {
 
-    var id = $(this).attr('id');                    // get the id
-    $.featherlight($('.popup-modal #item'), {});    // open the modal
+        var id = $(this).attr('id');                
+        let item = menuItems.find(function(obj){
+            return obj.id == id;
+        });
 
-    
-    for(var i = 0; i < menuItemData.length; i++){
-        if(menuItemData[i][0] === id){
-            var curr = menuItemData[i];             // find the rest of the item's data using the ID
-            break;
+        // If the item we clicked on is found in our data, then
+        // we can open the modal and populate it with the data 
+        // associated with that items id.
+        if (typeof item != "undefined") {
+            $.featherlight($('.popup-modal #item'), {});
+            var modal = '.featherlight-content #item';
+            $(modal + ' h1').html(item['title']);
+            $(modal + ' h2.cost').html(item['price']);
+            $(modal + ' .image').css('background-image', 'url("img/' + item['photoName'] + '.jpg")');
         }
-    }
-
-    //  Fill all the modal fields with relevant data
-    var modal = '.featherlight-content #item';
-    $(modal + ' h1').html(curr[1]);
-    $(modal + ' h2.cost').html(curr[2]);
-    $(modal + ' .image').css('background-image', 'url("img/' + curr[0] + '.jpg")');
-});
-
-// ---------------------------------
-//      button pressing code
-// ---------------------------------
-
-// When the "order" button is clicked
-$("#order").click( function() {
-    console.log("order");
-});
-
-// When the help button is clickded
-$("#help").click( function() {
-    console.log("help!");
-    if($("#help-screen").is(":visible")){
-        $("help-screen").fadeOut();
-    } else {
-        $("#help-screen").fadeIn();
-    }
-});
-$("#help-screen #overlay").click( function(){
-    $("#help-screen").fadeOut();
-});
-
-function init() {
-    // Fill the menu with all the items
-    menuItemData.forEach(function(element) {
-        var id = element[0];        // item ID
-        var title = element[1];     // item title
-        var price = element[2];     // item price
-        
-        // Create the main list
-        $("#start").append('<div class="col-sm-4 parent"><div class="item" id="' + id + '">' + 
-         '<div class="photo" style="background-image: url(img/'+ id + '.jpg)"></div>' + 
-         '<div class="text">' + '<h1>' +  title + '</h1>' + 
-         '<h2>' + price + '</h2>' + "</div></div>");
-    
     });
-}
 
+    // Order button
+    $("#order").click( function() {
+        console.log("order");
+    });
 
+    // Help Button
+    $("#help").click( function() {
+        console.log("help!");
+        if($("#help-screen").is(":visible")){
+            $("help-screen").fadeOut();
+        } else {
+            $("#help-screen").fadeIn();
+        }
+    });
+
+    // When the modal is displayed, check if 
+    // we have clicked outside of the modal to 
+    // display the content
+    $("#help-screen #overlay").click( function(){
+        $("#help-screen").fadeOut();
+    });
+
+    // ---------------------------------
+    //      Initialization code
+    // ---------------------------------
+    function init() {
+        let entrees = menuItems.filter(function(item){
+            return item['category'] == 'entree';
+        });
+
+        let desserts = menuItems.filter(function(item){
+            return item['category'] == 'dessert';
+        });
+
+        let appetizers = menuItems.filter(function(item){
+            return item['category'] == 'appetizers';
+        });
+
+        let drinks = menuItems.filter(function(item){
+            return item['category'] == 'drinks';
+        });
+
+        entrees.forEach(function(item) {
+            let id = item['id'];
+            let photoName = item['photoName'];
+            let title = item['title'];
+            let price = item['price'];
+
+            $("#start").append('<div class="col-sm-4 parent"><div class="item" id="' + id + '">' + 
+                '<div class="photo" style="background-image: url(img/'+ photoName + '.jpg)"></div>' + 
+                '<div class="text">' + '<h1>' +  title + '</h1>' + 
+                '<h2>' + price + '</h2>' + "</div></div>");
+        });
+    }
 });
--- js/vars.js
+++ js/vars.js
@@ -1,6 +1,55 @@
-var menuItemData = [
-    ['hamburger', 'Hamburger', 14.99, 'entree'],
-    ['blt', 'BLT', 9.99, 'entree'],
-    ['steak', 'Steak', 19.99, 'entree'],
-    ['steak-sandwich', 'Steak Sandwich', 14.99, 'entree'],
+const menuItems = [
+    {
+        'id' : 0,
+        'title' : 'Hamburger',
+        'photoName' : 'hamburger',
+        'price' : 14.99,
+        'category' : 'entree',
+        'description' : 'A nice juicy hamburger....',
+        'facts' : {
+            'calories' : 450,
+            'Sodium' : 430, // mg 
+            'protein' : 20, // g
+        }
+    },
+    {
+        'id' : 1,
+        'title' : 'BLT',
+        'photoName' : 'blt',
+        'price' : 9.99,
+        'category' : 'entree',
+        'description' : 'A nice juicy hamburger....',
+        'facts' : {
+            'calories' : 450,
+            'Sodium' : 430, // mg 
+            'protein' : 20, // g
+        }
+    },
+    {
+        'id' : 2,
+        'title' : 'Steak',
+        'photoName' : 'steak',
+        'price' : 19.99,
+        'category' : 'entree',
+        'description' : 'A nice juicy hamburger....',
+        'facts' : {
+            'calories' : 450,
+            'Sodium' : 430, // mg 
+            'protein' : 20, // g
+        }
+    },
+    {
+        'id' : 3,
+        'title' : 'Ice Cream',
+        'photoName' : 'ice-cream',
+        'price' : 'free',
+        'category' : 'dessert',
+        'description' : 'It is dope',
+        'facts' : {
+            'calories' : 0,
+            'Sodium' : 0, // mg 
+            'protein' : 0, // g
+        }
+    }
+
 ];
